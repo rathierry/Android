c0d7c34f5abc3904eb6358f9452f4f8dd85df6da
Thierry <t.ravelonjanahary@gmail.com>
2018-08-29 00:37:32 +0400

    UserLoginActivity w.i.p

--- LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/UserLoginActivity.java
+++ LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/UserLoginActivity.java
@@ -1,10 +1,7 @@
 package com.team.lezomadetana.activity;
 
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.team.lezomadetana.R;
 
@@ -12,7 +9,7 @@ import com.team.lezomadetana.R;
  * Created by team on 28/08/2018.
  **/
 
-public class SplashActivity extends BaseActivity {
+public class UserLoginActivity extends BaseActivity {
 
     // ===========================================================
     // Constants
@@ -37,25 +34,7 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-        );
-        setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        setContentView(R.layout.activity_user_login);
     }
 
     // ===========================================================
