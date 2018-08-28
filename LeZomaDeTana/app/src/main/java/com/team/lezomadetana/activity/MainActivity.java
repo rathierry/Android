4403aa2dad00e3adbb7f348552f442af68887439
Thierry <t.ravelonjanahary@gmail.com>
2018-08-28 22:29:57 +0400

    Adding packages architectures

--- LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/MainActivity.java
+++ LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.team.lezomadetana;
+package com.team.lezomadetana.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.team.lezomadetana.R;
 
 public class MainActivity extends AppCompatActivity {
 
