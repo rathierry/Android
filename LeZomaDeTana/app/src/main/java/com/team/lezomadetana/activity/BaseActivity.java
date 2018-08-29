33fb5de0702bde65e528c632cb912bbd498e2540
Thierry <t.ravelonjanahary@gmail.com>
2018-08-29 13:56:18 +0400

    User forgot Password UI (w.i.p)

--- LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/BaseActivity.java
+++ LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/BaseActivity.java
@@ -1,13 +1,21 @@
 package com.team.lezomadetana.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.team.lezomadetana.R;
+
+import java.util.List;
 
 /**
  * Created by team on 28/08/2018.
@@ -20,12 +28,14 @@ public class BaseActivity extends AppCompatActivity {
     // ===========================================================
 
     public static int SPLASH_TIME_OUT = 2000;
+    public static int LOADING_TIME_OUT = 3000;
 
     // ===========================================================
     // Fields
     // ===========================================================
 
     private Context _context;
+    private ProgressDialog _progressDialog;
     private Dialog _dialog;
     private Typeface _font;
 
@@ -63,6 +73,54 @@ public class BaseActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 
+    // ===========================================================
+    // Methods for Interfaces
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Private Methods
+    // ===========================================================
+
+    /**
+     * Getting the Phone Number, IMEI, and SIM Card ID
+     */
+    @SuppressLint("MissingPermission")
+    protected void getSimCardInfo() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            SubscriptionManager sManager = (SubscriptionManager) _context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+            List<SubscriptionInfo> subsInfoList = sManager.getActiveSubscriptionInfoList();
+            Toast.makeText(_context, "Current list = " + subsInfoList, Toast.LENGTH_LONG).show();
+            for (SubscriptionInfo subscriptionInfo : subsInfoList) {
+                if (subscriptionInfo != null) {
+                    String number = subscriptionInfo.getNumber();
+                    Toast.makeText(_context, "SIM number = " + number, Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            TelephonyManager telephonyManager = (TelephonyManager) _context.getSystemService(Context.TELEPHONY_SERVICE);
+            // get the SIM card ID
+            String simID = telephonyManager.getSimSerialNumber();
+            // get the phone number
+            String telNumber = telephonyManager.getLine1Number();
+            // get the IMEI number
+            String IMEI = telephonyManager.getDeviceId();
+
+            if (simID != null) {
+                Toast.makeText(this, "SIM card ID: " + simID, Toast.LENGTH_LONG).show();
+            }
+            if (telNumber != null) {
+                Toast.makeText(this, "Phone number: " + telNumber, Toast.LENGTH_LONG).show();
+            }
+            if (IMEI != null) {
+                Toast.makeText(this, "IMEI number: " + IMEI, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     /**
      * Overrides the pending Activity transition by performing the "Enter" animation.
      */
@@ -77,17 +135,26 @@ public class BaseActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
     }
 
-    // ===========================================================
-    // Methods for Interfaces
-    // ===========================================================
+    /**
+     * Enable spinner
+     */
+    protected void showLoadingView(String message) {
+        this.hideLoadingView();
+        _progressDialog = new ProgressDialog(BaseActivity.this, R.style.AppTheme_Dark_Dialog);
+        _progressDialog.setIndeterminate(true);
+        _progressDialog.setMessage(message);
+        _progressDialog.show();
+    }
 
-    // ===========================================================
-    // Public Methods
-    // ===========================================================
-
-    // ===========================================================
-    // Private Methods
-    // ===========================================================
+    /**
+     * Disable spinner
+     */
+    protected void hideLoadingView() {
+        if (_progressDialog != null && _progressDialog.isShowing()) {
+            _progressDialog.dismiss();
+        }
+        _progressDialog = null;
+    }
 
     // ===========================================================
     // Inner Classes/Interfaces
--- LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/UserLoginActivity.java
+++ LeZomaDeTana/app/src/main/java/com/team/lezomadetana/activity/UserLoginActivity.java
@@ -1,8 +1,16 @@
 package com.team.lezomadetana.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.team.lezomadetana.R;
@@ -21,16 +29,19 @@ public class UserLoginActivity extends BaseActivity {
     // Constants
     // ===========================================================
 
-    private static final int REQUEST_SIGNUP = 0;
-
     // ===========================================================
     // Fields
     // ===========================================================
 
-    //@BindView(R.id.user) EditText username;
-    //@BindView(R.id.pass) EditText password;
     //@BindString(R.string.login_error) String loginErrorMessage;
-    @BindView(R.id.user_login_btn_validate) Button _btnLogIn;
+    @BindView(R.id.user_login_relativeLayout)
+    RelativeLayout _layout;
+    @BindView(R.id.user_login_input_phone)
+    EditText _phoneText;
+    @BindView(R.id.user_login_input_password)
+    EditText _passwordText;
+    @BindView(R.id.user_login_btn_validate)
+    Button _btnLogIn;
 
     // ===========================================================
     // Constructors
@@ -44,30 +55,93 @@ public class UserLoginActivity extends BaseActivity {
     // Methods from SuperClass
     // ===========================================================
 
-    @OnClick(R.id.user_login_btn_validate)
-    void login() {
-        //
-    }
-
-    @OnClick(R.id.user_login_btn_register)
-    void submit() {
-        // start the SignUp activity
-        Intent intent = new Intent(getApplicationContext(), UserRegisterActivity.class);
-        startActivityForResult(intent, REQUEST_SIGNUP);
-        overridePendingTransitionEnter();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
         ButterKnife.bind(this);
+        getSimCardInfo();
+
+        // phone edit text
+        _phoneText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // TODO
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 10) {
+                    View view = UserLoginActivity.this.getCurrentFocus();
+                    if (view != null) {
+                        // hide keyboard
+                        /*InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);*/
+                        // take focus on password edit text
+                        _passwordText.requestFocus();
+                    }
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // TODO
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        _layout.requestFocus();
+        _phoneText.setError(null);
+        _passwordText.setError(null);
     }
 
     @Override
     public void onBackPressed() {
         // disable going back to the MainActivity
-        moveTaskToBack(true);
+        moveTaskToBack(false);
+    }
+
+    @OnClick(R.id.user_login_btn_validate)
+    void submit() {
+        // validate form
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        // disable login button
+        _btnLogIn.setEnabled(false);
+
+        // show spinner
+        showLoadingView("Authentication .....");
+
+        // save form inouts
+        String email = _phoneText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        // hide spinner
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // on complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                    }
+                }, LOADING_TIME_OUT);
+    }
+
+    @OnClick(R.id.user_login_btn_register)
+    void showUserRegisterActivity() {
+        // start the SignUp activity
+        startActivity(new Intent(getApplicationContext(), UserRegisterActivity.class));
+        overridePendingTransitionEnter();
+    }
+
+    @OnClick(R.id.user_login_textView_forgot_password)
+    void showUserForgotPasswordActivity() {
+        Toast.makeText(this, "work in progress", Toast.LENGTH_SHORT).show();
     }
 
     // ===========================================================
@@ -79,13 +153,43 @@ public class UserLoginActivity extends BaseActivity {
     // ===========================================================
 
     public void onLoginSuccess() {
+        _layout.requestFocus();
+        _phoneText.setText("");
+        _passwordText.setText("");
+        _phoneText.setError(null);
+        _passwordText.setError(null);
         _btnLogIn.setEnabled(true);
-        finish();
+        hideLoadingView();
     }
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
         _btnLogIn.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+        String phone = _phoneText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // phone number
+        if (phone.isEmpty() || TextUtils.isEmpty(phone) || !(phone.matches("^[0-9-]+$")) || phone.length() != 10) {
+            _phoneText.setError("mampidira laharana finday (tarehimarika 10)");
+            _phoneText.requestFocus();
+            valid = false;
+        }
+        // password
+        else if (password.isEmpty()) {
+            _passwordText.setError("mampidira teny miafina");
+            _passwordText.requestFocus();
+            valid = false;
+        } else {
+            _phoneText.setError(null);
+            _phoneText.clearFocus();
+            _passwordText.setError(null);
+            _passwordText.clearFocus();
+        }
+        return valid;
     }
 
     // ===========================================================
