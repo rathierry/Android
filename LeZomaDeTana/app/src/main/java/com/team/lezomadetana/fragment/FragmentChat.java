d15511393aed6a13d1de710db538472c5586ab18
hery <heryandoniaina@gmail.com>
2018-09-12 14:38:17 +0300

    vola

--- LeZomaDeTana/app/src/main/java/com/team/lezomadetana/fragment/FragmentChat.java
+++ LeZomaDeTana/app/src/main/java/com/team/lezomadetana/fragment/FragmentChat.java
@@ -45,12 +45,7 @@ public class FragmentChat extends BaseFragment {
     // Fields
     // ===========================================================
 
-    TextView madCoin;
-    TextView madCointText;
-    ImageView telma;
-    ImageView orange;
-    ImageView airtel;
-    UserCredentialResponse user;
+
 
 
 
@@ -77,29 +72,7 @@ public class FragmentChat extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        user = ((BaseActivity) getActivity()).getCurrentUser(getActivity());
 
-
-       /* View view = inflater.inflate(R.layout.fragment_chat, container, false);
-        madCoin = view.findViewById(R.id.madcoin_text);
-        madCointText = view.findViewById(R.id.madcoin_text);
-        telma = view.findViewById(R.id.m_vola_item);
-        orange = view.findViewById(R.id.orange_money_item);
-        airtel = view.findViewById(R.id.airtel_money_item);
-
-
-        telma.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "a", Toast.LENGTH_LONG);
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse(Uri.parse("tel:" + "*#06") + Uri.encode("#")));
-                startActivity(intent);
-            }
-        });
-
-
-        refreshMadcoin();*/
 
         return inflater.inflate(R.layout.fragment_chat, container, false);
     }
@@ -108,66 +81,10 @@ public class FragmentChat extends BaseFragment {
     public void onPrepareOptionsMenu(Menu menu) {
         super.onPrepareOptionsMenu(menu);
 
-        MenuItem itemMenuSearch = menu.findItem(R.id.action_search);
-        MenuItem itemMenuPayment = menu.findItem(R.id.action_payment);
-        MenuItem itemMenuInfo = menu.findItem(R.id.action_information);
-
-        itemMenuSearch.setEnabled(false);
-        itemMenuSearch.setVisible(false);
-        itemMenuPayment.setEnabled(false);
-        itemMenuPayment.setVisible(false);
-        itemMenuInfo.setEnabled(false);
-        itemMenuInfo.setVisible(false);
-    }
-
-    public void refreshMadcoin() {
-        APIInterface api = APIClient.getClient(BaseActivity.ROOT_MDZ_USER_API).create(APIInterface.class);
-
-        // create basic authentication
-        String auth = BasicAuth();
-
-        // send query
-        Call<JsonObject> call = api.getAllWallet(auth);
-
-        // request
-        call.enqueue(new Callback<JsonObject>() {
-            @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                if (response.code() == 200) {
-                    JsonArray filter = response.body().get("_embedded").getAsJsonObject().get("userWallets").getAsJsonArray();
-                    List<Wallet> wallets = null;
-
-                    if (filter.size() > 0) {
-                        wallets = new ArrayList<Wallet>();
-                        for (int i = 0; i < filter.size(); i++) {
-                            Wallet wallet = new Gson().fromJson(filter.get(i), Wallet.class);
-                            wallets.add(wallet);
-
-                        }
-
-
-                        for (int i = 0; i < wallets.size(); i++) {
-                            if (wallets.get(i).getUserId().equals(user.getId())) {
-                                madCointText.setText("" + wallets.get(i).getBalance());
-                                break;
-                            }
-
-                        }
-
-                    }
-
-
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-
-            }
-        });
 
     }
+
+
 
     // ===========================================================
     // Methods for Interfaces
--- LeZomaDeTana/app/src/main/java/com/team/lezomadetana/fragment/FragmentPayment.java
+++ LeZomaDeTana/app/src/main/java/com/team/lezomadetana/fragment/FragmentPayment.java
@@ -1,15 +1,37 @@
 package com.team.lezomadetana.fragment;
 
+import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.team.lezomadetana.R;
+import com.team.lezomadetana.activity.BaseActivity;
+import com.team.lezomadetana.api.APIClient;
+import com.team.lezomadetana.api.APIInterface;
+import com.team.lezomadetana.model.receive.UserCredentialResponse;
+import com.team.lezomadetana.model.receive.Wallet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.team.lezomadetana.activity.BaseActivity.BasicAuth;
 
 /**
  * Created by RaThierry on 11/09/2018.
@@ -24,6 +46,10 @@ public class FragmentPayment extends BaseFragment {
     // ===========================================================
     // Fields
     // ===========================================================
+
+    private  TextView money;
+    private UserCredentialResponse user;
+    private LinearLayout paymentCharge;
 
     // ===========================================================
     // Constructors
@@ -46,6 +72,21 @@ public class FragmentPayment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View mView = inflater.inflate(R.layout.fragment_payment, container, false);
+        money = (TextView) mView.findViewById(R.id.madcoin_text);
+        final BaseActivity activity =  ((BaseActivity)getActivity());
+        user = activity.getCurrentUser(activity);
+        paymentCharge = (LinearLayout) mView.findViewById(R.id.payement_charge);
+
+        paymentCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity,"AMPIDITRA CLICKED",Toast.LENGTH_LONG);
+            }
+        });
+
+        refreshMadcoin();
         return inflater.inflate(R.layout.fragment_payment, container, false);
     }
 
@@ -76,6 +117,55 @@ public class FragmentPayment extends BaseFragment {
     // ===========================================================
     // Private Methods
     // ===========================================================
+    public void refreshMadcoin() {
+        APIInterface api = APIClient.getClient(BaseActivity.ROOT_MDZ_USER_API).create(APIInterface.class);
+
+        // create basic authentication
+        String auth = BasicAuth();
+
+        // send query
+        Call<JsonObject> call = api.getAllWallet(auth);
+
+        // request
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.code() == 200) {
+                    JsonArray filter = response.body().get("_embedded").getAsJsonObject().get("userWallets").getAsJsonArray();
+                    List<Wallet> wallets = null;
+
+                    if (filter.size() > 0) {
+                        wallets = new ArrayList<Wallet>();
+                        for (int i = 0; i < filter.size(); i++) {
+                            Wallet wallet = new Gson().fromJson(filter.get(i), Wallet.class);
+                            wallets.add(wallet);
+
+                        }
+
+
+                        for (int i = 0; i < wallets.size(); i++) {
+                            if (wallets.get(i).getUserId().equals(user.getId())) {
+                                money.setText("Vola ao anaty vata :" + wallets.get(i).getBalance() + " Ar");
+
+                                break;
+                            }
+
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
+            }
+        });
+
+    }
 
     // ===========================================================
     // Inner Classes/Interfaces
